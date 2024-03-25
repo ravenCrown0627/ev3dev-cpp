be33a3427a578deed762d29057cae9899b387356
ravenCrown0627 <60862845+ravenCrown0627@users.noreply.github.com>
2024-03-25 17:33:04 +0800

    Add feature robot move in a rectangle

--- src/robot_movement.cpp
+++ src/robot_movement.cpp
@@ -21,12 +21,14 @@ public:
   ~control();
 
   void drive(int speed, int time=0);
-  void turn(int direction);
+  void turn(int direction, int speed=0);
   void stop();
   void reset();
+  int move_in_centimeter(int speed, int distance);
 
   bool initialized() const;
   void terminate() { _terminate = true; }
+
 
   state _state;
 
@@ -53,10 +55,12 @@ control::control() :
 // Destructor
 control::~control()
 {
-  stop();
   reset();
 }
 
+// Drive the robot forward or backward
+// The unit of speed is in tacho/sec, +ve value is forward, -ve value is backward
+// The unit of time is in milliseconds
 void control::drive(int speed, int time)
 {
   _motor_left.set_speed_sp(speed);
@@ -82,7 +86,17 @@ void control::drive(int speed, int time)
   }
 }
 
-void control::turn(int direction)
+// Turn the robot either left or right
+// The unit of direction is in degree, +ve value is right, -ve value is left
+// The unit of speed is in tacho/sec
+//
+// Formula to calculate the direction requried:
+//    R_wheel * Theta_wheel = R_robot * Theta_robot
+//    R_wheel     : Radius of the wheel
+//    Theta_wheel : Angle of the wheel needed to turn
+//    R_robot     : Radius of the robot (half of the distance between the two wheels)
+//    Theta_robot : Angle of the robot needed to turn
+void control::turn(int direction, int speed)
 {
   if (_state != state_idle)
     stop();
@@ -92,8 +106,13 @@ void control::turn(int direction)
 
   _state = state_turning;
 
-  _motor_left. set_position_sp( direction).set_speed_sp(500).run_to_rel_pos();
-  _motor_right.set_position_sp(-direction).set_speed_sp(500).run_to_rel_pos();
+  // Reset the motor position
+  _motor_left .set_position_sp(0);
+  _motor_right.set_position_sp(0);
+
+  // Run the motors to the relative position
+  _motor_left. set_position_sp( direction).set_speed_sp(speed).run_to_rel_pos();
+  _motor_right.set_position_sp(-direction).set_speed_sp(speed).run_to_rel_pos();
 
   while (_motor_left.state().count("running") || _motor_right.state().count("running"))
     this_thread::sleep_for(chrono::milliseconds(10));
@@ -101,6 +120,7 @@ void control::turn(int direction)
   _state = state_idle;
 }
 
+// Stop the robot with the hold action
 void control::stop()
 {
   _motor_left .set_stop_action("hold").stop_action();
@@ -109,6 +129,7 @@ void control::stop()
   _state = state_idle;
 }
 
+// Reset the status of the motor to the default parameter
 void control::reset()
 {
   if (_motor_left.connected())
@@ -127,30 +148,41 @@ bool control::initialized() const
 }
 
 // speed is in degrees per second, distance is in cm
-void move_in_centimeter(int speed, int distance)
+int control::move_in_centimeter(int speed, int distance)
 {
+  double circumference = 17.8; // cm
+  double speed_in_cms = speed / 360.0 * circumference;
+  
+  int time = distance / speed_in_cms * 1000;
+
+  cout <<"\n\n\nTime: " << time << " ms\n";
+
+  return time;
+}
+
+int main()
+{
+  const int speed     = 180;
+  const int distance  = 20;
+
   control robot_ctrl;
 
   if (!robot_ctrl.initialized())
     return -1;
 
-  double circumference = 17.8; // cm
-  double speed_in_cms = speed / 360.0 * circumference;
-  int time = distance / speed_in_cms * 1000;
+  int time = robot_ctrl.move_in_centimeter(speed, distance);
+  cout << "\n###Time: " << time << " ms" << endl;
 
-  robot_ctrl.drive(speed, time);
-}
+  // Calculate the time it takes to drive x meter
+  for (int i = 0; i < 4; i++)
+  {
+    robot_ctrl.drive(speed, time);
+    robot_ctrl.turn(191, 300);   // 194 degrees, 300 degrees/sec
+  }
 
-int main()
-{
-  // control robot_ctrl;
+  // Allowing the robot to move further a bit to avoid slipping curve
+  robot_ctrl.drive(180, 100); // 180 degrees/sec, 0.1 seconds
 
-  // if (!robot_ctrl.initialized())
-  //   return -1;
-  
-  // // Calculate the time it takes to drive x meter
-  // robot_ctrl.drive(180);
-
-  move_in_centimeter(90, 100);  // 100 cm forward at 90 speed
+  robot_ctrl.stop();
   return 0;
 }
