57dc9fc678ccb844a61b2db10eedd3ab23f91ae7
Laleylaa <itslaleyla@gmail.com>
2024-06-17 23:15:58 +0800

    Complete version of frontend codes

--- src/assignment_8/input.py
+++ src/assignment_8/input.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import os
-import math
 from time import sleep
 
 from ev3dev2.display import Display
@@ -11,15 +10,6 @@ class InputManager:
     def __init__(self):   
         self.display = Display()
         self.button = Button()
-        # self.inputs = {
-        #     "x": 0,
-        #     "y": 0,
-        #     # 1 is manhattan distance, 2 is euclidean distance
-        #     "mode": 1
-        # }
-        # self.input_mode = "x" # set the initial input mode to x
-        # self.input_mode_list = ["x", "y", "mode"]
-        # Set 1 as initial input value
         self.value = 1
 
         #initialize the button
@@ -39,50 +29,28 @@ class InputManager:
         self.display.update()
         pass
 
-        # if self.input_mode == "x":
-        #     self.print_text("Please enter x: ", 0, 64)
-        # elif self.input_mode == "y":
-        #     self.print_text("Please enter y: ", 0, 64)
-        # elif self.input_mode == "mode":
-        #     self.print_text("Please enter mode: ", 0, 64)
-        #     self.print_text("0: Manhattan", 0, 74)
-        #     self.print_text("1: Euclidean", 0, 84)
-
     def increase(self, state):
         if not state:
             self.value += 1
             # Only allow input from 1-5
-            self.value = self.value % 6
+            if self.value > 5: 
+                self.value = 1
+            if self.value < 1: 
+                self.value = 5
             self.refresh_display()
-
-            # self.value += 1
-            # if self.input_mode == "x":
-            #     # x from 0-4
-            #     self.value = self.value % 5
-            # elif self.input_mode == "y":
-            #     # y from 0-3
-            #     self.value = self.value % 4
-            # elif self.input_mode == "mode":
-            #     # mode from 0-1
-            #     self.value = self.value % 2                     
+                    
         return
 
     def decrease(self, state):
         if not state:
             self.value -= 1
             # Only allow input from 1-5
-            self.value = self.value % 6
+            if self.value > 5: 
+                self.value = 1
+            if self.value < 1: 
+                self.value = 5
             self.refresh_display()
 
-            # if self.input_mode == "x":
-            #     # x from 0-4
-            #     self.value = self.value % 5
-            # elif self.input_mode == "y":
-            #     # y from 0-3
-            #     self.value = self.value % 4
-            # elif self.input_mode == "mode":
-            #     # mode from 0-1
-            #     self.value = self.value % 2
         return
     
 
@@ -93,16 +61,21 @@ if __name__ == "__main__":
     # Request a input
     menu = InputManager()
     menu.print_text("Compile Done!", 0, 0)
+    menu.display.update()
     sleep(2)    
 
     # Save the input
     while not menu.button.enter: 
-        menu.button.procee()
+        menu.refresh_display()
+        menu.button.process()
         sleep(0.01)
+    exit = menu.value
+    menu.display.clear()
     menu.print_text("Input Done!", 0, 0)
-    menu.print_text("Robot will leave at exit {}".format(menu.value), 0, 10)
+    menu.print_text("Robot will leave at exit {}".format(exit), 0, 10)
+    menu.display.update()
     sleep(1)
 
     # # Call command to move robot
-    # command = 
-    # os.system(command)
+    command = "./test_ultrasonic {}".format(exit)
+    os.system(command) 
